b6d102d151e0d82e22e45c18fdd3fa6f11a0be15
Cong Nguyen <papovietnam5@gmail.com>
2023-09-16 09:15:35 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vocab/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     react()
   ],
